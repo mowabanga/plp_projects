db131de4b37c3680614cc7bf9a05b39886774c1d
mowabanga <itsmundia@gmail.com>
2024-02-24 15:50:54 +0300

    Basic python

---
+++ python_tasks/user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("What's your current location? ")
+
+print(f"Hello {name.title()}, you are {age} years old and live in {location.title()}.")
